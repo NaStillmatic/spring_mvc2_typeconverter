4ce7bd8024ff4c8fac296f20aab3897d21dd7216
Joe <nastillmatic@gmail.com>
2024-01-26 18:22:44 +0900

    Section 10 - 컨버전 서비스 - ConversionService

--- src/main/java/hello/typeconverter/converter/IntegerToStringConverter.java
+++ src/main/java/hello/typeconverter/converter/IntegerToStringConverter.java
@@ -4,9 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.converter.Converter;
 
 @Slf4j
-public class IntegerToStringConverter implements Converter {
+public class IntegerToStringConverter implements Converter<Integer, String> {
     @Override
-    public Object convert(Object source) {
+    public String convert(Integer source) {
         log.info("convert source={}", source);
         return String.valueOf(source);
     }
